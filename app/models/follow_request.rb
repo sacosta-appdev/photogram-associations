00db27db20acd3845eb2096d629fe8e619fa038a
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2022-11-03 01:17:25 +0000

    Update comments

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -11,29 +11,11 @@
 #
 
 class FollowRequest < ApplicationRecord
-  # validates(:sender, { :presence => true})
-  # validates(:recipient, {
-  #   :presence => true,
-  #   :uniqueness => { :scope => [:sender_id] }
-  # })
+  # Association accessor methods to define:
+  
+  ## Direct associations
 
-  # def sender
-  #   my_sender_id = self.sender_id
+  # FollowRequest#sender: returns a row from the users table associated to this follow request by the sender_id column
 
-  #   matching_users = User.where({ :id => my_sender_id })
-
-  #   the_user = matching_users.at(0)
-
-  #   return the_user
-  # end
-
-  # def recipient
-  #   my_recipient_id = self.recipient_id
-
-  #   matching_users = User.where({ :id => my_recipient_id })
-
-  #   the_user = matching_users.at(0)
-
-  #   return the_user
-  # end
+  # FollowRequest#recipient: returns a row from the users table associated to this follow request by the recipient_id column
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,23 +22,37 @@ class User < ApplicationRecord
   ## Direct associations
 
   # User#comments: returns rows from the comments table associated to this user by the author_id column
+
   # User#own_photos: returns rows from the photos table  associated to this user by the owner_id column
+
   # User#likes: returns rows from the likes table associated to this user by the fan_id column
+
   # User#sent_follow_requests: returns rows from the follow requests table associated to this user by the sender_id column
+
   # User#received_follow_requests: returns rows from the follow requests table associated to this user by the recipient_id column
+
 
   ### Scoped direct associations
 
   # User#accepted_sent_follow_requests: returns rows from the follow requests table associated to this user by the sender_id column, where status is 'accepted'
+
   # User#accepted_received_follow_requests: returns rows from the follow requests table associated to this user by the recipient_id column, where status is 'accepted'
   
+
   ## Indirect associations
 
-  # User#liked_photos: returns rows from the photos table associated to this user through their likes
-  # User#commented_photos: returns rows from the photos table associated to this user through their comments
+  # User#liked_photos: returns rows from the photos table associated to this user through its likes
 
-  # User#followers: returns rows from the users table associated to this user through their accepted_received_follow_requests
-  # User#leaders: returns rows from the users table associated to this user through their accepted_sent_follow_requests
-  # User#feed: returns rows from the photos table associated to this user through their leaders (the leaders' own_photos)
-  # User#discover: returns rows from the photos table associated to this user through their leaders (the leaders' liked_photos)
+  # User#commented_photos: returns rows from the photos table associated to this user through its comments
+
+
+  ### Indirect associations built on scoped associations
+
+  # User#followers: returns rows from the users table associated to this user through its accepted_received_follow_requests (the follow requests' senders)
+
+  # User#leaders: returns rows from the users table associated to this user through its accepted_sent_follow_requests (the follow requests' recipients)
+
+  # User#feed: returns rows from the photos table associated to this user through its leaders (the leaders' own_photos)
+
+  # User#discover: returns rows from the photos table associated to this user through its leaders (the leaders' liked_photos)
 end
